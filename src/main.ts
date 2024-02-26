5300a26b438cc8d80b65db37e74b208ad2cdfc3c
s0a1e2 <oops2198@gmail.com>
2024-02-26 15:48:07 +0330

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useWebSocketAdapter(new IoAdapter(app));
   app.enableCors();
+  const ioAdapter = new IoAdapter(app);
+  await ioAdapter.createIOServer(8089, { cors: { origin: '*' }});
+  app.useWebSocketAdapter(ioAdapter);
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
